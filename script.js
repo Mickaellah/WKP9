34c9bba213ce68fd3cb6cc6ac8ddfd2bcf8da80f
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-08-29 10:56:28 +0300

    Fetched the movies link and created a function to generate it into HTML, added some styles for them.

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-console.log("It works!!!!");
+const movieList = document.querySelector('.movies');
 
+
+async function fetchMovie() {
+    const response = await fetch('https://ghibliapi.herokuapp.com/films', {
+        headers: {
+            Accept: 'application/json',
+        }
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+
+async function displayMovies() {
+    const movies = await fetchMovie();
+    const html = movies.map(movie => {
+        return `
+        <article class="movie">
+            <header class="heading">
+                <h2 class="header">${movie.title}</h2>
+                <p class="release_date">${movie.release_date}</p>
+                <p class="score">${movie.rt_score}</p>
+            </header>
+            <div>
+                <p class="description">${movie.description}</p>
+            </div>
+            <div class="responsible">
+                <p class="director">${movie.director}</p>
+                <p class="producer">${movie.producer}</p>
+            </div>
+        </article>
+        `;
+    });
+    console.log(html);
+    movieList.innerHTML = html.join('');
+}
+
+displayMovies();
